02aa3fd82a6221ad14320bfba326c57b45715512
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-05-09 19:13:20 -0300

    Correction in the API model

--- angular/src/app/services/pokemon.service.service.ts
+++ angular/src/app/services/pokemon.service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  description: string;
+  height: number;
+  weight: number;
+  types: string[];
+  stats: { [key: string]: number };
+  abilitys: string[];
+  moves: string[];
+  evolution: string[];  
+  sprite: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class PokemonServiceService {
+export class PokemonService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/pokemons';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPokemons(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(this.apiUrl);
+  }
+
+  getPokemonById(id: number): Observable<Pokemon> {
+    return this.http.get<Pokemon>(`${this.apiUrl}/${id}`);
+  }
 }
--- spring-boot/src/main/java/br/com/marcoscunha/PokedexApi/model/Pokemon.java
+++ spring-boot/src/main/java/br/com/marcoscunha/PokedexApi/model/Pokemon.java
@@ -19,7 +19,7 @@ public class Pokemon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false, length = 255)
     private String name;
